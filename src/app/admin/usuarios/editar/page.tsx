2570e8b9d415fb2bc6ae0d9da7c31751a54318d1
Luiz Clovis Dos Santos Matias <luizclovis2014@gmail.com>
2024-05-20 13:54:12 -0300

    Realizado retirada do diretório html

--- src/app/admin/usuarios/editar/page.tsx
+++ src/app/admin/usuarios/editar/page.tsx
@@ -5,33 +5,33 @@ import { useUsuarioService } from '../../../../services/usuario';
 import { Field, Form, Formik } from 'formik';
 import Link from 'next/link';
 
-export default function UsuarioEditarPage ({params}: any) {
+export default function UsuarioEditarPage({ params }: any) {
 
-    const usuariosSrv = useUsuarioService();
-    const [ mensagem , setMensagem ] = React.useState<null|boolean>(null)
-    // ======================================================================
-    const handleSalvar = async (usuario:any) => {
-      setMensagem(null);
-      const retorno = await usuariosSrv.cadastrar(usuario);
-      setMensagem(retorno.sucesso)
-    }
-    // ======================================================================
-    return (
-      <main>
-            <AdminHeader titulo={('Cadastrar Usuário')}/>
-            <h6>Formulário</h6>    
+  const usuariosSrv = useUsuarioService();
+  const [mensagem, setMensagem] = React.useState<null | boolean>(null)
+  // ======================================================================
+  const handleSalvar = async (usuario: any) => {
+    setMensagem(null);
+    const retorno = await usuariosSrv.cadastrar(usuario);
+    setMensagem(retorno.sucesso)
+  }
+  // ======================================================================
+  return (
+    <main>
+      <AdminHeader titulo={('Cadastrar Usuário')} />
+      <h6>Formulário</h6>
 
-            { mensagem != null && mensagem == false && <p className="alert alert-danger">Não foi possível cadastrar usuário</p>}
-            { mensagem != null && mensagem == true && <p className="alert alert-success">Cadastrado com sucesso</p>}
+      {mensagem != null && mensagem == false && <p className="alert alert-danger">Não foi possível cadastrar usuário</p>}
+      {mensagem != null && mensagem == true && <p className="alert alert-success">Cadastrado com sucesso</p>}
 
-          <Formik
-            initialValues={{  nome: '', email: '', senha: ''}}
-            enableReinitialize
-            onSubmit={handleSalvar}
-          >
-            {({isSubmitting}) => (
-              <Form>
-            <div className="card-body">  
+      <Formik
+        initialValues={{ nome: '', email: '', senha: '' }}
+        enableReinitialize
+        onSubmit={handleSalvar}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <div className="card-body">
               {/* NOME */}
               <div className="row">
                 <div className="col-md-12">
@@ -42,18 +42,18 @@ export default function UsuarioEditarPage ({params}: any) {
                 </div>
 
                 {/* EMAIL */}
-                <div className="col-md-4">
+                <div className="col-md-6">
                   <div className="form-group">
                     <label className="form-control-label">Email</label>
-                    <Field className="form-control" type="email"  name="email"/>
+                    <Field className="form-control" type="email" name="email" />
                   </div>
                 </div>
 
                 {/* SENHA */}
-                <div className="col-md-4">
+                <div className="col-md-6">
                   <div className="form-group">
                     <label className="form-control-label">Senha</label>
-                    <Field className="form-control" type="password" name="senha"/>
+                    <Field className="form-control" type="password" name="senha" />
                   </div>
                 </div>
 
@@ -63,20 +63,20 @@ export default function UsuarioEditarPage ({params}: any) {
                     <button className='btn btn-primary w-100' type="submit" disabled={isSubmitting}>Salvar</button>
                   </div>
                 </div>
+
+                {/* BOTÃO VOLTAR */}
+                <div className="col-md-12">
+                  <div className="form-group">
+                    <Link href={`/admin/usuarios/`} className="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Back form">
+                      Voltar
+                    </Link>
+                  </div>
+                </div>
+
               </div>
             </div>
-
-            {/* BOTÃO VOLTAR */}
-            <div className="col-md-12">
-                <div className="form-group">
-                  <Link href={`/admin/usuarios/`} className="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Back form">
-                    Voltar
-                  </Link>
-                </div>
-              </div>
-
-            </Form>)}   
-          </Formik>
-      </main>
-    );
+          </Form>)}
+      </Formik>
+    </main>
+  );
 }
